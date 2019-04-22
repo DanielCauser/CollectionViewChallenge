af4c671d3481a903c77638f97ac24ce5215f6c86
Daniel Causer <danielcauser@gmail.com>
2019-04-22 18:35:20 -0400

    [DC] Code so far

---
+++ CollectionViewChallenge/CollectionViewChallenge/Models/Post.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace CollectionViewChallenge.Models
+{
+    public class Post
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ CollectionViewChallenge/CollectionViewChallenge/ViewModels/CollectionViewChallengeViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using CollectionViewChallenge.Models;
+using Newtonsoft.Json;
+using Xamarin.Forms;
+using System.Collections.ObjectModel;
+
+namespace CollectionViewChallenge.ViewModels
+{
+    public class CollectionViewChallengeViewModel : BaseViewModel
+    {
+        public CollectionViewChallengeViewModel()
+        {
+            _posts = new List<string> { "Causer" };
+            //_posts = new List<Post> { new Post { Name = " Causer" } };
+        }
+
+        private List<string> _posts;
+        public List<string> Posts
+        {
+            get => _posts;
+            set => SetProperty(ref _posts, value);
+        }
+
+        public ICommand LoadCommand => new Command(() => OnLoadCommand());
+
+        public async void OnLoadCommand()
+        {
+            await RunSafeAsync(async () =>
+            {
+                await LoadPosts();
+                IsRefreshing = false;
+            });
+        }
+
+        public override async void LoadData()
+        {
+            await RunSafeAsync(async () =>
+            {
+                await LoadPosts();
+            });
+        }
+
+        public async Task LoadPosts()
+        {
+            //using (var httpClient = new HttpClient())
+            //{
+            //    var result = await httpClient.GetStringAsync("https://jsonplaceholder.typicode.com/posts");
+            //    Posts = JsonConvert.DeserializeObject<List<Post>>(result);
+            //}
+        }
+    }
+}
